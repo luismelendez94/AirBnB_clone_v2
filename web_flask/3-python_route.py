2e889389c0ed4845c1c4a46a304c9d75a25da889
luismelendez94 <2709@holbertonschool.com>
2021-09-15 15:48:57 +0000

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ Display a message """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def holby():
+    """ Display a message """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Display message followed by <text> """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """ Display message followed by <text> """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
