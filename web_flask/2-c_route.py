bd0b482a5ac49a3268eb0bedc4a68ea5061f384a
luismelendez94 <2709@holbertonschool.com>
2021-09-15 15:09:03 +0000

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ Display a message """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def holby():
+    """ Display a message """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Display message followed by <text> """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
