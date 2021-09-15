ea1b8da9b809cc34cc1279c6be96b01b8640cf6a
luismelendez94 <2709@holbertonschool.com>
2021-09-15 20:02:30 +0000

    Task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application
+"""
+
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """ Display a message """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def holby():
+    """ Display a message """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """ Display message followed by <text> """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """ Display message followed by <text> """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    """ Display message if n is integer """
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def num_temp(n):
+    """ Display HTML if n in integer """
+    if isinstance(n, int):
+        return render_template('/5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
