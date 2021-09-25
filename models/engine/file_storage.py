1e27c52b86b31459dae7c77ccf58fc070115f4fa
luismelendez94 <2709@holbertonschool.com>
2021-09-25 05:31:08 +0000

    Task 6

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,7 @@ class FileStorage:
         key = obj.__class__.__name__ + '.' + obj.id
         if key in self.__objects:
             del self.__objects[key]
+
+    def close(self):
+        """ Deserialice the JSON file to objects """
+        reload()
